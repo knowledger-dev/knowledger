fc1b4fe2e3c4a0ef6527c92e778a25c0e8c4dbf9
Jantomz <zjaden28@gmail.com>
2024-10-18 23:01:21 -0400

    Added quick changes to NavBar

--- frontend/src/molecules/NavBar.jsx
+++ frontend/src/molecules/NavBar.jsx
@@ -6,7 +6,7 @@ import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 export default function NavBar() {
   const { user } = useAuthContext();
-  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(true);
 
   const { logout } = useLogout();
   const navigate = useNavigate();
@@ -20,41 +20,41 @@ export default function NavBar() {
   };
 
   return (
-    <nav className="flex fixed flex-col justify-center items-center font-inter font-bold z-50 gap-6 top-0 m-4 w-[5%]">
+    <>
       <button
         onClick={toggleCollapse}
-        className="text-black outline-lavender dark:text-white no-underline hover:text-gray-800 active:text-gray-700 dark:hover:text-gray-300 dark:active:text-gray-400 text-center rounded-md cursor-pointer"
+        className=" z-50 outline-lavender p-5  no-underline text-center rounded-md cursor-pointer fixed top-0 left-0"
       >
         {isCollapsed ? (
-          <AiOutlineMenu size={25} />
+          <div className="text-black dark:text-white">
+            <AiOutlineMenu size={25} />
+          </div>
         ) : (
-          <AiOutlineClose size={25} />
+          <div className="text-white">
+            <AiOutlineClose size={25} />
+          </div>
         )}
       </button>
-      {!isCollapsed && (
-        <>
+      <nav
+        className={`fixed bg-russianviolet flex flex-col font-inter font-bold gap-6 justify-start items-center top-0 left-0 h-full z-30 transition-transform duration-300 w-[250px] ${
+          isCollapsed ? "-translate-x-full" : "translate-x-0"
+        }`}
+      >
+        <button
+          onClick={() => handleButtonClick("/")}
+          className="dark:text-white  hover:text-gray-300 active:text-gray-400  text-white py-6 no-underline  cursor-pointer text-center rounded-md p-4"
+        >
+          Agent
+        </button>
+        {user && (
           <button
-            onClick={() => handleButtonClick("/")}
-            className="dark:text-white hover:text-gray-900 active:text-gray-800 text-black py-2 no-underline dark:hover:text-gray-300 dark:active:text-gray-400 cursor-pointer text-center rounded-md p-4"
+            onClick={logout}
+            className="dark:text-white  hover:text-gray-300 active:text-gray-400  text-white py-6 no-underline  cursor-pointer text-center rounded-md p-4"
           >
-            Agent
+            Logout
           </button>
-          {user && (
-            <button
-              onClick={logout}
-              className="dark:text-white hover:text-gray-900 active:text-gray-800 text-black py-2 no-underline dark:hover:text-gray-300 dark:active:text-gray-400 cursor-pointer text-center rounded-md p-4"
-            >
-              Logout
-            </button>
-          )}
-          {/* <button
-            onClick={() => handleButtonClick("/settings")}
-            className="text-white py-2 no-underline hover:text-gray-300 active:text-gray-800 cursor-pointer"
-          >
-            <AiOutlineSetting size={25} />
-          </button> */}
-        </>
-      )}
-    </nav>
+        )}
+      </nav>
+    </>
   );
 }
